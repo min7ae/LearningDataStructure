23e89a1230e9587ab9ec0d782001e2dc92b72362
min7ae <innersme@gmail.com>
2019-07-05 23:33:36 +0900

    문제 1463 top-down 방식 추가

--- Baekjoon/p1463.cpp
+++ Baekjoon/p1463.cpp
@@ -4,38 +4,48 @@
  */
 
 #include <iostream>
+#define MIN(X,Y) ((X) < (Y) ? (X) : (Y)) 
+
 using namespace std;
 
 const int MAX = 1000000;
-int D[MAX] = {0, };
 
-// 최소 함수
-int min(int a, int b){
-    return a > b ? b : a;
-}
+int D[MAX] = {0, };
+int d[MAX] = {0, };
 
 // 1로 만들기 함수 f
-int makeOne(int n){
-    if (n == 1)
-    {
-        D[1] = 0;
-        return 0;
+int makeOne(int n){    
+    if(n == 1) return 0;
+    if(D[n] > 0) return D[n];
+    if (n % 6 == 0) {
+        D[n] = MIN(MIN(makeOne(n/3), makeOne(n/2)) , makeOne(n-1)) + 1;
+    } else if (n % 3 == 0) {
+        D[n] = MIN(makeOne(n/3), makeOne(n-1)) + 1;
+    } else if (n % 2 == 0) {
+        D[n] = MIN(makeOne(n/2) , makeOne(n-1)) + 1;
+    } else {
+        D[n] = makeOne(n-1) + 1;
     }
-    
-    if (n % 3 == 0)
-    {
-        D[n] = min(makeOne(n-1), makeOne(n/3)) + 1;
-        return D[n];
-    }
-
-    if (n % 2 == 0)
-    {
-        D[n] = min(makeOne(n-1), makeOne(n/2)) + 1;
-        return D[n];
-    }
-    
-    D[n] = makeOne(n-1) + 1;
     return D[n];
+}
+
+// top-down 방식
+int makeOnev2(int n){
+    d[1] = 0;
+    for (int i = 2; i <= n; i++)
+    {
+        d[i] = d[i-1] + 1;
+        if (i % 2 == 0 && d[i] > d[i/2] + 1)
+        {
+            d[i] = d[i/2] + 1;
+        }
+        if (i % 3 == 0 && d[i] > d[i/3] + 1)
+        {
+            d[i] = d[i/3] + 1
+        }
+        
+    }
+    
 }
 
 int main(){
