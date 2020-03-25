dbafe85b5a226b61e44e84151a6272cd0a79d9f7
minjae <innersme@gmail.com>
2020-03-25 22:38:07 +0900

    오름차순 후 모든 합

--- Baekjoon/p11399.cpp
+++ Baekjoon/p11399.cpp
@@ -4,8 +4,15 @@
 */
 
 #include <iostream>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
 
+bool asc(int a, int b)
+{
+    return a < b;
+}
 
 int main()
 {
@@ -14,21 +21,36 @@ int main()
         각 사람이 걸리는 시간 p[n];
      */
     int n;
-    int p[1001];
-    
-    // 입력 끝
+    vector<int> v;
+
     cin >> n;
-    for(int i = 1; i <= n; i++)
+
+    for(int i = 0; i < n; i++)
     {
-        cin >> p[i];
+        int timetoWithdraw;
+        cin >> timetoWithdraw;
+        v.push_back(timetoWithdraw);
     }
 
-    /* 최소값 출력 */
-    /*
-        
-    
-    */
+    /* 오름차순 */
+    sort(v.begin(), v.end() );
 
+    /*
+        1. 오름차순 정렬
+        2. a번째 있는 수는 1부터 a까지 있는 숫자를 모두 더한 값이 자신이 기다린 시간
+        3. 각자 시간을 n명 다 더한다.
+     */
+    int r = 0;
+    for(int i=0; i<n; i++)
+    {
+        int innerR = 0;
+        for(int j = 0; j <= i; j++)
+        {
+            innerR += v[j];
+        }
+        r += innerR;
+    }
+    printf("%d", r);
 
     return 0;
 }
