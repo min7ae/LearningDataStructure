e78bf1c615afe88d0419c631431066d2e5c773af
minjae <innersme@gmail.com>
2020-03-25 22:19:03 +0900

    문제 11399 입력 값 추가

---
+++ Baekjoon/p11399.cpp
@@ -0,0 +1,34 @@
+/*
+    ATM
+    https://www.acmicpc.net/problem/11399
+*/
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    /*
+        사람의 수: n
+        각 사람이 걸리는 시간 p[n];
+     */
+    int n;
+    int p[1001];
+    
+    // 입력 끝
+    cin >> n;
+    for(int i = 1; i <= n; i++)
+    {
+        cin >> p[i];
+    }
+
+    /* 최소값 출력 */
+    /*
+        
+    
+    */
+
+
+    return 0;
+}
